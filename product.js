b6678e5a1aaaa528d44e191520df65b0b2af2f30
Caroline Wulff <carolineisinchina@gmail.com>
2021-02-09 23:22:22 +0100

    product updated: JS fixed, css/html corrected

--- product.js
+++ product.js
@@ -1,51 +1,25 @@
 const urlParams = new URLSearchParams(window.location.search);
-//grab URL in id and store its value in id
 const id = urlParams.get("id");
 console.log(id);
 
 //const id = 1163;
 const url = "https://kea-alt-del.dk/t7/api/products/" + id;
-//fetch data
-fetch(url)
-  .then((res) => res.json())
-  .then((data) => showProduct(data));
-//populate page
-
-function showProduct(product) {
-  console.log(product);
-
-  document.querySelector(
-    "img.productimage"
-  ).src = `https://kea-alt-del.dk/t7/images/webp/1000${product.id}.webp`;
-  document.querySelector("img.productimage").alt = product.productdisplayname;
-}
-
-/*
-const urlParams = new URLSearchParams(window.location.search);
-const id = urlParams.get("id");
-console.log(id);
-
-//const id = 1163;
-const url = "https://kea-alt-del.dk/t7/api/products/2801" + id;
 //fetch the data
 fetch(url)
   .then((res) => res.json())
   .then((data) => showProduct(data));
-
 //populate the page
 function showProduct(product) {
   console.log(product);
-  document.querySelector(".product_info .productName").textContent =
+
+  document.querySelector(".breadcrumbs .brand").textContent = product.brandname;
+  document.querySelector(".breadcrumbs .productname").textContent =
     product.productdisplayname;
-  document.querySelector(".product_info .colour").textContent =
-    product.basecolour;
+  document.querySelector(".productinfo .productname").textContent =
+    product.productdisplayname;
 
-  //image changes
   document.querySelector(
-    "img.productImage"
+    "img.productimage"
   ).src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
-
-  document.querySelector("img.productImage").alt = product.productdisplayname;
+  document.querySelector("img.productimage").alt = product.productdisplayname;
 }
-
-*/
